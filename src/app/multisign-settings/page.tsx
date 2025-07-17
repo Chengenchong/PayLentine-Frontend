d37074164c8fe842de144bc7a6eb1330df05c702
Chengenchong <145186137+Chengenchong@users.noreply.github.com>
2025-07-18 02:20:39 +0800

    refactor: simplify MultiSignSettingsPage and enhance SignInContent with useLogin hook

--- src/app/multisign-settings/page.tsx
+++ src/app/multisign-settings/page.tsx
@@ -2,15 +2,13 @@
 import React from 'react';
 import Header from '../../components/LoginHeader';
 import Footer from '../../components/Footer';
-import Content, { MultiSignProvider } from '../../pages/MultiSign-Settings/Content';
+import Content from '../../pages/MultiSign-Settings/Content';
 
 export default function MultiSignSettingsPage() {
   return (
     <>
       <Header />
-      <MultiSignProvider>
-        <Content />
-      </MultiSignProvider>
+      <Content />
       <Footer />
     </>
   );
